90ca96b2486314882107de2eff41dbfb5a5001c9
mav01-code <marreddyakshayavarshini@gmail.com>
2026-01-22 22:43:27 +0530

    API endpoint changes

--- PasswordStrengthChecker/app.py
+++ PasswordStrengthChecker/app.py
@@ -49,4 +49,4 @@ def check_password():
     return jsonify({"strength": total_percentage})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
